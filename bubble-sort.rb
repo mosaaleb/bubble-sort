782f7cc757e480a17ff1d9fc12cea7e7fe5bd699
kvnlay <kingstevens007@gmail.com>
2019-03-21 14:42:05 +0100

    Linter error fixes

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,37 +1,31 @@
 #!/usr/bin/ruby
 
-def bubble_sort (array)
-    n = array.length
-    swapped = true 
+def bubble_sort(array)
+    swapped = true
     while swapped
         j = 0
         swapped = false
-        while j < n - 1
-            if array[j] > array[j+1] 
-                array[j], array[j+1] = array[j+1], array[j]
+        while j < array.length - 1
+            if array[j] > array[j + 1]
+                array[j], array[j + 1] = array[j + 1], array[j]
                 swapped = true
             end
             j += 1
         end
     end
-    return array
 end
 
 def bubble_sort_by(array)
-    n = array.length
     swapped = true
     while swapped
         i = 0
         swapped = false
-        while i < n - 1
+        while i < array.length - 1
             if yield(array[i], array[i + 1]) > 0
-                array[i], array[i+1] = array[i+1], array[i]
+                array[i], array[i + 1] = array[i+1], array[i]
                 swapped = true
             end
             i += 1
         end
     end
-    return array
 end
-
-
