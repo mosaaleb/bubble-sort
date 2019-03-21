bf071e584523c439878beb392819e5dc3827d188
kvnlay <kingstevens007@gmail.com>
2019-03-21 14:25:48 +0100

    added bubble_sort_by method

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,21 +1,37 @@
+#!/usr/bin/ruby
+
 def bubble_sort (array)
     n = array.length
-    i = 0
-    while i < n
+    swapped = true 
+    while swapped
         j = 0
+        swapped = false
         while j < n - 1
             if array[j] > array[j+1] 
                 array[j], array[j+1] = array[j+1], array[j]
+                swapped = true
             end
             j += 1
         end
-        i += 1
+    end
+    return array
+end
+
+def bubble_sort_by(array)
+    n = array.length
+    swapped = true
+    while swapped
+        i = 0
+        swapped = false
+        while i < n - 1
+            if yield(array[i], array[i + 1]) > 0
+                array[i], array[i+1] = array[i+1], array[i]
+                swapped = true
+            end
+            i += 1
+        end
     end
     return array
 end
 
 
-a = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 ]
-
-bubble_sort(a)
-p a
