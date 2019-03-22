d9cca838553606b7ebbd9277c89651747d5c236f
mosaaleb <muhammed.ebeid@gmail.com>
2019-03-22 11:47:03 +0200

    hopefully last linter fixes

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/ruby
 
-def bubble_sort (array)
-  swapped = true 
+def bubble_sort(array)
+  swapped = true
   while swapped
     swapped = false
     (array.length - 1).times do |i|
-      if array[i] > array[i + 1] 
+      if array[i] > array[i + 1]
         array[i], array[i + 1] = array[i + 1], array[i]
         swapped = true
       end
@@ -14,32 +14,24 @@ def bubble_sort (array)
 end
 
 def bubble_sort_by(array)
-swapped = true
-while swapped
-swapped = false
-(array.length - 1).times do |i|
-if yield(array[i], array[i + 1]) > 0
-array[i], array[i + 1] = array[i + 1], array[i]
-swapped = true
-end
-end
-end
-end
-
-
-
-# tests
-
-
-a = ["hi","hello","hey", "lkjsdf'", "lkjsdklfjsldjfklsjdlfjslkdf", "klfsdjklfjskldjflkjskldfklsjdklfj"]
-b = [1, 234,4324 ,992, 2345234,9875982739]
-bubble_sort_by(a) do |left,right|
-  left.length - right.length
+  swapped = true
+  while swapped
+    swapped = false
+    (array.length - 1).times do |i|
+      if yield(array[i], array[i + 1]) > 0
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swapped = true
+      end
+    end
+  end
 end
 
-
-p a
-
-
-bubble_sort(b)
-p b
+### TESTS
+# a = ["hi","hello","hey", "lkjsdf'", "lkjsdklfjsldjfklsjdlfjslkdf", "klfsdjklfjskldjflkjskldfklsjdklfj"]
+# b = [1, 234, 432 ,992, 234]
+# bubble_sort_by(a) do |left,right|
+# left.length - right.length
+# end
+# p a
+# bubble_sort(b)
+# p b
